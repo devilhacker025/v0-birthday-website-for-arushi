909421a50be2284ffd9425ad8bbdc714c8494053
yash srivastava <yashsrivastava1808@gmail.com>
2025-10-03 22:31:54 +0200

    edit with tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Arushi Singh',
+  description: 'Created with heart',
+  generator: 'yash srivastava',
 }
 
 export default function RootLayout({
